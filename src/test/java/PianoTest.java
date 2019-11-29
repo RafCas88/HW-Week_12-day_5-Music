61b9a084c12c0dae2679322ea4e3ec7bfe61ba49
Raffaele Castaldo <raffaelecastaldo88@libero.it>
2019-11-29 14:10:43 +0000

    piano tested

--- src/test/java/PianoTest.java
+++ src/test/java/PianoTest.java
@@ -1,2 +1,26 @@
+import instruments.Piano;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PianoTest {
+
+    Piano piano;
+
+    @Before
+    public void before(){
+
+        piano = new Piano(24, "wood", "black", "electric");
+    }
+
+    @Test
+    public void hasKeys(){
+        assertEquals(24, piano.getKeys());
+    }
+
+    @Test
+    public void canPlay(){
+        assertEquals("Softly, in the dusk, a woman is singing to me;", piano.play());
+    }
 }
