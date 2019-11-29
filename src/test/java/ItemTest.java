79828ef39789335a6ad9fbbb9148cd551b6515b7
Raffaele Castaldo <raffaelecastaldo88@libero.it>
2019-11-29 16:32:16 +0000

    mvp point 5 completed

--- src/test/java/ItemTest.java
+++ src/test/java/ItemTest.java
@@ -22,5 +22,28 @@ public class ItemTest {
     @Test
     public void hasType(){
         assertEquals("guitar strings", item1.getType());
+        assertEquals("drum sticks", item2.getType());
+        assertEquals("sheet music", item3.getType());
+    }
+
+    @Test
+    public void hasBuyingPrice(){
+        assertEquals(5, item1.getBuyingPrice());
+        assertEquals(8, item2.getBuyingPrice());
+        assertEquals(3, item3.getBuyingPrice());
+    }
+
+    @Test
+    public void hasSellingPrice(){
+        assertEquals(10, item1.getSellingPrice());
+        assertEquals(16, item2.getSellingPrice());
+        assertEquals(6, item3.getSellingPrice());
+    }
+
+    @Test
+    public void hasMarkUp(){
+        assertEquals(5, item1.markUp());
+        assertEquals(8, item2.markUp());
+        assertEquals(3, item3.markUp());
     }
 }
